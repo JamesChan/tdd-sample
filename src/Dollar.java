496a005cf944067005afeed558ba8898450f28a4
James Chan <szjameschan@gmail.com>
2011-12-19 00:53:58 +0800

    初次提交

---
+++ src/Dollar.java
@@ -0,0 +1,17 @@
+
+public class Dollar {
+	private int amount;
+	
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+	
+	public Dollar times(int multiplier) {
+		return new Dollar(amount*multiplier);
+	}
+	
+	public boolean equals(Object object) {
+		Dollar dollar = (Dollar)object;
+		return dollar.amount == amount;
+	}
+}
