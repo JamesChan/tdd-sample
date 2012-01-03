4dec77ebe977802624e9e7d72c57d755e701cdbb
James Chan <szjameschan@gmail.com>
2012-01-03 17:52:50 +0800

    13章

--- src/Money.java
+++ src/Money.java
@@ -34,7 +34,11 @@ public class Money implements Expression {
 		return new Money(amount*multiplier, currency);
 	}
 	
-	public Expression plus(Money money) {
-		return new Money(amount + money.amount, currency);
+	public Expression plus(Money addend) {
+		return new Sum(this, addend);
+	}
+	
+	public Money reduce(String to) {
+		return this;
 	}
 }
